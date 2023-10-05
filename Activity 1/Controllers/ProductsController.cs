df7e6c589551ac9c279a72c4caf69119f632cc98
t.nugent <t.nugent@hotmail.co.uk>
2023-10-05 14:24:03 +0100

    Adding actions to the ProductsController, and creating views to match them. Passing parameters from Controllers to Views

--- Activity 1/Controllers/ProductsController.cs
+++ Activity 1/Controllers/ProductsController.cs
@@ -8,5 +8,17 @@ namespace Activity_1.Controllers
         {
             return View();
         }
+        // This action is run when /products/message is visited
+        public IActionResult Message()
+        {
+            return View();
+        }
+
+        public IActionResult Welcome(string name, int secretNumber=123)
+        {
+            ViewBag.Name = name;
+            ViewBag.SecretNumber = secretNumber;
+            return View();
+        }
     }
 }

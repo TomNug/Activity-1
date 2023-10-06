5b7a4063ee385a97ba755beaeac4b95e787b46d7
t.nugent <t.nugent@hotmail.co.uk>
2023-10-06 15:39:22 +0100

    Login page added as data entry. Login success/fail views added. Updated navbar to hold login.

---
+++ Activity 1/Controllers/LoginController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Activity_1.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult ProcessLogin(Models.UserModel userModel)
+        {
+            if (userModel.Username == "user" && userModel.Password == "pass")
+            {
+                return View("LoginSuccess", userModel);
+            } else
+            {
+                return View("LoginFailure", userModel);
+            }
+        }
+    }
+}

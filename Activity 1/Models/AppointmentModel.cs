48b076aee0e17b8a508f08360c57e9247a7c7eef
t.nugent <t.nugent@hotmail.co.uk>
2023-10-09 16:42:46 +0100

    Using DisplayNames on the Model to improve appearance in the view.

--- Activity 1/Models/AppointmentModel.cs
+++ Activity 1/Models/AppointmentModel.cs
@@ -1,11 +1,18 @@
-﻿namespace Activity_1.Models
+﻿using System.ComponentModel;
+
+namespace Activity_1.Models
 {
     public class AppointmentModel
     {
+        [DisplayName("Patient's Full Name")]
         public string PatientName { get; set; }
+        [DisplayName("Appointment Request Date")] 
         public DateTime AppointmentDate { get; set; }
+        [DisplayName("Patient's Approximate Net Worth")]
         public decimal PatientNetWorth { get; set; }
+        [DisplayName("Doctor's Last Name")]
         public string DoctorName { get; set; }
+        [DisplayName("Patient's Perceived Level of Pain (1-10)")]
         public int PainLevel { get; set; }
     }
 }

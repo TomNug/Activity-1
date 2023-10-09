02252c4a7ce7124e6c6285e6d2446b2435c3be7c
t.nugent <t.nugent@hotmail.co.uk>
2023-10-09 14:28:12 +0100

    Creating Security Services class. Holds known users. Can call the SecurityServices isValid function with a User to determine if they exist.

---
+++ Activity 1/Services/SecurityServices.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using Activity_1.Models;
+
+namespace Activity_1.Services
+{
+    public class SecurityServices
+    {
+        List<UserModel> knownUsers = new List<UserModel>();
+
+        public SecurityServices()
+        {
+            knownUsers.Add(new UserModel { Id = 0, Username = "Ban O'Bannon", Password = "screenplay" });
+            knownUsers.Add(new UserModel { Id = 1, Username = "Jerry Goldsmith", Password = "music" });
+            knownUsers.Add(new UserModel { Id = 2, Username = "Yaphet Kotto", Password = "Parker" });
+        }
+        public bool isValid(UserModel user)
+        {
+            return knownUsers.Any(x => x.Id == user.Id && x.Username == user.Username && x.Password == user.Password);
+        }
+    }
+}

b16ff8063734e7c11cc0af7f9ecd743bb964f997
t.nugent <t.nugent@hotmail.co.uk>
2023-10-09 15:57:18 +0100

    Query to setup initial data

---
+++ SQLQuery1.sql
@@ -0,0 +1,6 @@
+﻿insert into dbo.Users (USERNAME, PASSWORD) values ('Sigourney Weaver', 'Ripley')
+insert into dbo.Users (USERNAME, PASSWORD) values ('Tom Skerrit', 'Dallas')
+insert into dbo.Users (USERNAME, PASSWORD) values ('Harry Dean Stanton', 'Brett')
+insert into dbo.Users (USERNAME, PASSWORD) values ('Yaphet Kotto', 'Parker')
+
+select * from Users

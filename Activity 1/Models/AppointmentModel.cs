4bdff68084895076f453d2fe680f4b8b3c8b2012
t.nugent <t.nugent@hotmail.co.uk>
2023-10-09 16:35:58 +0100

    Created new Model for Appointments Created new Controller for Appointments Created Appointment Create View

---
+++ Activity 1/Models/AppointmentModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Activity_1.Models
+{
+    public class AppointmentModel
+    {
+        public string PatientName { get; set; }
+        public DateTime AppointmentDate { get; set; }
+        public decimal PatientNetWorth { get; set; }
+        public string DoctorName { get; set; }
+        public int PainLevel { get; set; }
+    }
+}
